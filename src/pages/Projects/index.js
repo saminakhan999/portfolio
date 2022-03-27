5a54516295c81218ad8b204ffe1e1af85cc3c31d
saminakhan999 <samina.khan9999@gmail.com>
2022-03-27 23:54:10 +0100

    positioned project carousel

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -5,20 +5,42 @@ import { Carousel } from "3d-react-carousal";
 
 let slides = [
   <a href="https://super-coders.netlify.app/">
-    <img src="https://i.postimg.cc/WNDyJCwm/supercoders.png" alt="1" />
+    <img
+      src="https://i.postimg.cc/WNDyJCwm/supercoders.png"
+      alt="1"
+    />
+    <a href="https://github.com/saminakhan999/super-coders">
+      <img
+        className="github"
+        src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+      />
+    </a>
   </a>,
   <a href="https://habitat-tracker.netlify.app/">
     <img
+
       src="https://i.postimg.cc/wjjS7vBF/Screenshot-2022-03-27-at-18-41-48.png"
       alt="2"
     />
+    <a href="https://github.com/MongoDon-tBother/habitat">
+      <img
+        className="github"
+        src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+      />
+    </a>
   </a>,
   <a href="https://ultimate-quiz-game.netlify.app/">
     <img
-      className="projectImage"
+
       src="https://i.postimg.cc/LRFbCC9X/Screenshot-2022-03-27-at-18-45-35.png"
       alt="3"
     />
+    <a href="https://github.com/u-s-c-l/i-t-t-w-s-d">
+      <img
+        className="github"
+        src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+      />
+    </a>
   </a>,
 ];
 
@@ -35,12 +57,14 @@ function Projects() {
         <div className="projectsTop">
           <h1 className="projectsTitle">My Projects</h1>
           <p className="projectsPara">
-            Here are some of my most recent projects! Click on any of the projects to visit the site or on the github icon to view its repository!
+            Here are some of my most recent projects! Click on any of the
+            projects to visit the site or on the github icon to view its
+            repository!
           </p>
           <br></br>
           <br></br>
           <br></br>
-          <Carousel slides={slides} autoplay={true} interval={2500} />
+            <Carousel slides={slides} autoplay={true} interval={3000} />
         </div>
       </div>
       <br></br>
