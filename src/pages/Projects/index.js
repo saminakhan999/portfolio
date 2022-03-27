d8197edbf86388d82e66dfe9366d524e1c582b52
saminakhan999 <samina.khan9999@gmail.com>
2022-03-27 18:47:19 +0100

    added project images:

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -1,4 +1,15 @@
 import React from "react";
+import SimpleImageSlider from "react-simple-image-slider";
+
+const images = [
+  { url: "https://i.postimg.cc/WNDyJCwm/supercoders.png" },
+  {
+    url: "https://i.postimg.cc/wjjS7vBF/Screenshot-2022-03-27-at-18-41-48.png",
+  },
+  {
+    url: "https://i.postimg.cc/LRFbCC9X/Screenshot-2022-03-27-at-18-45-35.png",
+  },
+];
 
 function Projects() {
   return (
@@ -11,6 +22,15 @@ function Projects() {
         <div className="projectsTop">
           <h1 className="projectsTitle">My Projects</h1>
           <p className="projectsPara">Coming soon</p>
+        </div>
+        <div className="slider">
+          <SimpleImageSlider
+            width={896}
+            height={504}
+            images={images}
+            showBullets={true}
+            showNavs={true}
+          />
         </div>
       </div>
     </section>
