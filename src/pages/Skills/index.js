3112873fa32528e9f8d931d8e14a7a86a0159e62
saminakhan999 <samina.khan9999@gmail.com>
2022-03-31 17:25:39 +0100

    changed mongo icon

--- src/pages/Skills/index.js
+++ src/pages/Skills/index.js
@@ -33,7 +33,7 @@ const items = [
   ></img>,
   <img
     className="item icon"
-    src="https://cdn.icon-icons.com/icons2/2415/PNG/512/mongodb_plain_wordmark_logo_icon_146423.png"
+    src="https://www.opc-router.de/wp-content/uploads/2021/03/mongodb_thumbnail.png"
   ></img>,
   <img
     className="item icon"
