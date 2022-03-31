740eb8d1b0a683cea6d36dfb4a4a4503382e6993
saminakhan999 <samina.khan9999@gmail.com>
2022-03-31 17:06:19 +0100

    experiences show on click

--- src/pages/Experience/index.js
+++ src/pages/Experience/index.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
+
 
 
 function Experience() {
+    const [showTutor, setShowTutor] = useState(false);
+    const onClickTutor = () => {
+      setShowTutor(true)
+      setShowCode(false)};
+    const [showCode, setShowCode] = useState(false);
+    const onClickCode = () => {
+      setShowCode(true)
+      setShowTutor(false);
+    };
   return (
     <section id="experience" className="experienceContainer">
       <br></br>
@@ -13,22 +23,34 @@ function Experience() {
           <h1 className="experienceTitle">My Work Experience</h1>
           {/* <p className="experiencePara">Coming soon</p> */}
         </div>
-        <span className="workbutton">Titanium</span>
-        <span className="workbutton">futureproof</span>
-        <div></div>
       </div>
-      <div id="tutor">
-        Titanium Tutors | Mathematics Tutor | June 2018 – January 2022 <br></br>
-        - Plan and develop lessons to provide Mathematics tuition to students
-        <br></br>- Monitor and communicate student progression and goals to
-        students and parents <br></br>- Utilise strong time management &
-        organisation skills to manage tuition schedule alongside studies
-      </div>
-      <br></br><br></br><br></br>
-      <div id="futureproof">
-        Futureproof | Tech Trainee | January 2022 – Present <br></br>
-        - 13-week bootcamp covering the main areas of web development including JavaScript, HTML/CSS, Databases, and Python and applying these skills to various projects every week
-      </div>
+      <span className="workbutton" onClick={onClickTutor}>
+        Titanium
+      </span>
+      <span className="workbutton" onClick={onClickCode}>
+        futureproof
+      </span>
+      <div></div>
+      <br></br>
+      <br></br>
+      <br></br>
+      {showTutor ? (
+        <span id="tutor">
+          Titanium Tutors | Mathematics Tutor | June 2018 – January 2022
+          <br></br>- Plan and develop lessons to provide Mathematics tuition to
+          students
+          <br></br>- Monitor and communicate student progression and goals to
+          students and parents <br></br>- Utilise strong time management &
+          organisation skills to manage tuition schedule alongside studies
+        </span>
+      ) : showCode ? (
+        <div id="futureproof">
+          Futureproof | Tech Trainee | January 2022 – Present <br></br>- 13-week
+          bootcamp covering the main areas of web development including
+          JavaScript, HTML/CSS, Databases, and Python and applying these skills
+          to various projects every week
+        </div>
+      ) : null}
     </section>
   );
 }
